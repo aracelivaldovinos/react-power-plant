51e563b96f1e2f61cf9963b9d5cc586aa4ebeb1a
aracelivaldovinos <84604175+aracelivaldovinos@users.noreply.github.com>
2021-08-31 09:27:48 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Project from './project';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import './css/styles.css';
+
