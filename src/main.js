5f1d89b79b341359d99212261b75279c2062ca0d
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-09-01 13:01:52 -0500

    add two store states for cactus and pitcher plants

--- src/main.js
+++ src/main.js
@@ -3,16 +3,45 @@
 import * as plant from './js/plant.js';
 import * as state from './js/state.js';
 import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './css/styles.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+const cactus = plant.cactus("Henry the cactus");
+const pitcherPlant = plant.pitcherPlant("Callie");
+const cactusStateControl = state.storeState();
+const pitcherStateControl = state.storeState();
 
 
 $(document).ready(function() {
-
+  $('#cactus-name').text(cactus.name);
+  $('#pitcher-name').text(pitcherPlant.name);
   
+  // Cactus jQuery
   $('#feed').click(function() {
-    const newState = state.stateControl(plant.superFeed);
-    $('#soil-value').text(`Soil: ${newState.soil}`);
+    const cactusState = cactusStateControl(plant.feed);
+    $('#cactus-soil').text(`Soil: ${cactusState.soil}`);
+  });
+  $('#hydrate').click(function() {
+    const cactusState = cactusStateControl(plant.hydrate);
+    $('#cactus-water').text(`Water: ${cactusState.water}`);
+  });
+  $('#superLight').click(function() {
+    const cactusState = cactusStateControl(plant.superLight);
+    $('#cactus-light').text(`Light: ${cactusState.light}`);
+  });
+
+  // Pitcher plant
+  $('#superFeed').click(function() {
+    const pitcherState = pitcherStateControl(plant.superFeed);
+    $('#pitcher-soil').text(`Soil: ${pitcherState.soil}`);
+  });
+  $('#superHydrate').click(function() {
+    const pitcherState = pitcherStateControl(plant.superWater);
+    $('#pitcher-water').text(`Water: ${pitcherState.water}`);
+  });
+  $('#light').click(function() {
+    const pitcherState = pitcherStateControl (plant.light);
+    $('#pitcher-light').text(`Light: ${pitcherState.light}`);
   });
 });
